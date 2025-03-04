78f28041c9f4efbf05f1c302694205dc67eb6765
akhmelev <akhmelev@gmail.com>
2025-03-04 17:29:27 +0300

    ROLL BACK (del deploy). see branch deploy

--- src/main/java/com/javarush/khmelov/config/ApplicationProperties.java
+++ src/main/java/com/javarush/khmelov/config/ApplicationProperties.java
@@ -1,6 +1,7 @@
 package com.javarush.khmelov.config;
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.FileReader;
 import java.net.URI;
@@ -9,6 +10,7 @@ import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Properties;
 
+@Slf4j
 public class ApplicationProperties extends Properties {
 
     public static final String HIBERNATE_CONNECTION_URL = "hibernate.connection.url";
@@ -18,7 +20,29 @@ public class ApplicationProperties extends Properties {
 
     @SneakyThrows
     public ApplicationProperties() {
-        this.load(new FileReader(CLASSES_ROOT + "/application.properties"));
+        String applicationPropertiesFile = "/application.properties";
+        log.info("Loading application properties from file: {}", applicationPropertiesFile);
+        this.load(new FileReader(CLASSES_ROOT + applicationPropertiesFile));
+        this.forEach((key, val) -> {
+            String value = val.toString();
+            if (value.startsWith("${")) {
+                String[] valueData = value
+                        .replace("${", "")
+                        .replace("}", "")
+                        .split(":", 2);
+                if (valueData.length == 2) {
+                    log.info("Default dev property '{}' to '{}'", key, valueData[0]);
+                    value = valueData[1];
+                }
+                String envValue = System.getProperties().getProperty(valueData[0]);
+                if (envValue != null) {
+                    log.info("Enviroment dev property '{}' to '********'", key);
+                    value = envValue;
+                }
+                setProperty(key.toString(), value);
+                log.info("Setting property '{}'", key);
+            }
+        });
         try {
             String driver = this.getProperty(HIBERNATE_CONNECTION_DRIVER_CLASS);
             Class.forName(driver);
---
+++ src/main/webapp/WEB-INF/start-page.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@include file="head.jsp"%>
+<body>
+<h1><%= "Hello World!" %>
+</h1>
+<br/>
+<a href="list-user">List Users</a>
+</body>
